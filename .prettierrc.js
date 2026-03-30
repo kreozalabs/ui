b8ced40096470d5d5686ad46626c9a92503de23d
Nurzhan Muratkhan <nurzhanmuratkhan@gmail.com>
2026-03-30 22:15:39 +0500

    feat: initialize component library project with build, lint, and release automation

---
+++ .prettierrc.js
@@ -0,0 +1,1 @@
+module.exports = require("@kreozalabs/prettier-config");
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// The main entry point for @kreozalabs/ui
+
+// Export utility functions
+export * from "./lib/utils";
+
+// Export ready-to-use React components (to be implemented)
+// export * from "./components/button"
