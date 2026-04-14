4523512f4e384ded138be980851fb496d56e5b34
Nurzhan Muratkhan <nurzhanmuratkhan@gmail.com>
2026-04-14 18:05:49 +0500

    chore: migrate to ESM and update ESLint configuration with react-refresh support

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,33 @@
-import kreozalabsConfig from "@kreozalabs/eslint-config";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default [
-  ...kreozalabsConfig
-];
+export default defineConfig([
+  globalIgnores(["**/dist/**", "**/node_modules/**"]),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  eslintConfigPrettier,
+]);
