1a2df067c19bde91b1ede1ae7f85271eccae0dbe
Nurzhan Muratkhan <nurzhanmuratkhan@gmail.com>
2026-04-17 15:57:29 +0500

    chore: configure typescript-eslint project service in ui package eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,11 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import eslintConfigPrettier from "eslint-config-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig([
   globalIgnores(["**/dist/**", "**/node_modules/**"]),
@@ -15,6 +20,10 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
