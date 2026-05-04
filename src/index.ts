bbb21bdb47fc533022f38493cc58f0993eff013b
Nurzhan Muratkhan <nurzhanmuratkhan@gmail.com>
2026-05-04 23:12:05 +0500

    feat: add Select component based on Radix UI to the component library

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,4 @@ export * from "./components/ui/label";
 export * from "./components/ui/badge";
 export * from "./components/ui/sheet";
 export * from "./components/ui/textarea";
+export * from "./components/ui/select";
